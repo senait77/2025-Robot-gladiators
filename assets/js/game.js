a6ed75e0c830c530514aeb7278b04fd351f660dd
senait77 <senutekie77@gmail.com>
2025-08-27 17:42:07 -0500

    start and end game function setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,34 +1,19 @@
-// creating a user side player
-
-var playerName = window.prompt("What is your robot's name");
+var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// you can also  log multiple values at once like this
-
-console.log(playerName, playerHealth , playerAttack);
-
-
-// creating an enemy from the developer side
-
-var enemyNames = ["Roborto", "Amy Android" , "Robo Trumble"];
-console.log(enemyNames);
-console.log(enemyNames.length);
-
-// i =i+1 loop through the array until it is done
-for(var i = 0; i < enemyNames.length; i++) {
-  console.log(enemyNames[i]);
-  console.log(i);
-  console.log(enemyNames[i] + 'is at ' + i + ' index.');
-}
+var enemyNames = ['Roborto', 'Amy Android', 'Robo Trumble'];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-console.log ( enemyNames, enemyHealth , enemyAttack);
+console.log(enemyNames);
+console.log(enemyNames.length);
+console.log(enemyNames[0]);
+console.log(enemyNames[3]);
 
-
-        var fight = function(enemyName) {
+// fight function (now with parameter for enemy's name)
+var fight = function(enemyName) {
   while (playerHealth > 0 && enemyHealth > 0) {
     // ask player if they'd like to fight or run
     var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
@@ -43,7 +28,7 @@ console.log ( enemyNames, enemyHealth , enemyAttack);
         window.alert(playerName + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerMoney for skipping
         playerMoney = playerMoney - 10;
-        console.log("playerMoney", playerMoney)
+        console.log("playerMoney", playerMoney);
         break;
       }
     }
@@ -81,23 +66,58 @@ console.log ( enemyNames, enemyHealth , enemyAttack);
     } else {
       window.alert(playerName + ' still has ' + playerHealth + ' health left.');
     }
+  } // end of while loop
+}; // end of fight function
+
+//function to start a new game
+var startGame = function() { 
+
+  // reset player stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+
+   for (var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+
+      var pickedEnemyName = enemyNames[i];
+
+      enemyHealth = 50;
+
+      fight(pickedEnemyName);
+    }
+    else {
+      window.alert("You have lost your robot in battle! Game Over!");
+      break;
+    }
   }
+  endGame();
 };
 
-//fight(); replacing the fight fiunctio call
+// start the game when the page loads
+startGame();
 
-for (var i= 0 ; i < enemyNames.length ; i++) {
- var pickedEnemyName = enemyNames[i];
- enemyHealth = 50;
+//function to end the entire game
+var endGame = function() {
+   // if player is still alive, player wins!
+  if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+  } 
+  else {
+    window.alert("You've lost your robot in battle.");
+  }
+
+   //ask the player if they would like to play agian 
+var playAgainConfirm = window.confirm("Would you like to play again?");
  
-  fight(pickedEnemyName); 
+if(playAgainConfirm) {
+
+  //restart the game
+  startGame();
+} else {
+  window.alert('Thankyou for playing robot Gladiatiors! come back soon!');
 }
- 
 
-
-
-
-
-
-
+};
 
